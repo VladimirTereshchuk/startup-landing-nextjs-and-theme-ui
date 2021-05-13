150d5707a3a58fef942ce956eab8f2caee497694
VladimirTereshchuk <vofca1974@gmail.com>
2021-05-13 08:55:52 +0300

    first commit

---
+++ src/components/link.js
@@ -0,0 +1,16 @@
+/** @jsxImportSource theme-ui */
+
+import { jsx, NavLink as MenuLink, Link as A } from "theme-ui";
+import NextLink from "next/link";
+
+export function NavLink({ path, label, children, ...rest }) {
+  return <h1>NavLink</h1>;
+}
+
+export function Link({ path, label, children, ...rest }) {
+  return (
+    <A {...rest} href={path}>
+      {children || label}
+    </A>
+  );
+}
